c8c93df86bd22bc08b871ff8b0ce453fe64a0d5b
UPN <n00213501@upn.pe>
2024-04-03 19:02:37 -0500

    asd

---
+++ app/src/main/java/dev/didnt/desmov/s2/InterfazActivity.kt
@@ -0,0 +1,11 @@
+package dev.didnt.desmov.s2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InterfazActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_interfaz)
+    }
+}
---
+++ app/src/main/java/dev/didnt/desmov/s2/RelativeActivity.kt
@@ -0,0 +1,11 @@
+package dev.didnt.desmov.s2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RelativeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_relative)
+    }
+}
