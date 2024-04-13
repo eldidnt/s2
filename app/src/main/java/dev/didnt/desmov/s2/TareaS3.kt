01542e5c76bb99da15cba162a94dcee463696d9a
eldidnt <luiscervantes.pe@gmail.com>
2024-04-13 10:49:14 -0500

    S3 HomeWork added

---
+++ app/src/main/java/dev/didnt/desmov/s2/TareaS3.kt
@@ -0,0 +1,20 @@
+package dev.didnt.desmov.s2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class TareaS3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_tarea_s3)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
